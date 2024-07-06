eca8e1fe6418f3a5bc8f0c7cfb182640f09238e9
xcd0 <xcd0@xcd0.com>
2024-07-06 15:30:50 +0900

    tmp

--- src/main.go
+++ src/main.go
@@ -11,18 +11,19 @@ import (
 //go:generate go run ./script/prebuild/prebuild.go ./keymap.hjson
 //go:generate go run ./script/gen-def/main.go ./vial.json
 
+var (
+	gpioPin []machine.Pin
+)
+
 func init() {
 	usb.Product = "sss46g-0.0.2"
-}
-
-func main() {
-	d := keyboard.New()
-
-	gpioPin := []machine.Pin{
+	// 全てのGPIOにjスライスとしてアクセスできるようにしておく。
+	gpioPin = []machine.Pin{
 		machine.GPIO0, machine.GPIO1, machine.GPIO2, machine.GPIO3, machine.GPIO4, machine.GPIO5, machine.GPIO6, machine.GPIO7, machine.GPIO8, machine.GPIO9,
 		machine.GPIO10, machine.GPIO11, machine.GPIO12, machine.GPIO13, machine.GPIO14, machine.GPIO15, machine.GPIO16, machine.GPIO17, machine.GPIO18, machine.GPIO19,
 		machine.GPIO20, machine.GPIO21, machine.GPIO22, machine.GPIO23, machine.GPIO24, machine.GPIO25, machine.GPIO26, machine.GPIO27, machine.GPIO28, machine.GPIO29,
 	}
+	// すべてのGPIOをプルアップしておく。
 	for i, _ := range gpioPin {
 		gpioPin[i].Configure(
 			machine.PinConfig{
@@ -30,8 +31,13 @@ func main() {
 			},
 		)
 	}
+}
+
+func main() {
+	d := keyboard.New()
 	row := gpioPin[0:4]                      // 0,1,2,3
 	col := append(gpioPin[4:9], gpioPin[17]) // 4,5,6,7,8,17
+
 	d.AddMatrixKeyboard(col, row, GetKeycodes())
 	loadKeyboardDef()
 	d.Loop(context.Background())
